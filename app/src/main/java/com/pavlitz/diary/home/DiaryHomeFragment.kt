a36eb7f20cb9784e26285e447b22e760fd961c03
MrWhite <sergeymalek23@gmail.com>
2022-12-11 21:20:20 +0500

    changed content of home fragment slightly changed style of app

--- app/src/main/java/com/pavlitz/diary/home/DiaryHomeFragment.kt
+++ app/src/main/java/com/pavlitz/diary/home/DiaryHomeFragment.kt
@@ -1,5 +1,6 @@
-package com.pavlitz.diary
+package com.pavlitz.diary.home
 
 import androidx.fragment.app.Fragment
+import com.pavlitz.diary.R
 
 class DiaryHomeFragment: Fragment(R.layout.diary_fragment_layout)
