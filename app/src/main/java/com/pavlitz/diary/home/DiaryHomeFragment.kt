03163631a186c9ed32b50cccf0906ad7416eccf3
MrWhite <sergeymalek23@gmail.com>
2022-12-12 13:21:55 +0500

    add floating action button

--- app/src/main/java/com/pavlitz/diary/home/DiaryHomeFragment.kt
+++ app/src/main/java/com/pavlitz/diary/home/DiaryHomeFragment.kt
@@ -1,6 +1,20 @@
 package com.pavlitz.diary.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.pavlitz.diary.R
 
-class DiaryHomeFragment: Fragment(R.layout.diary_fragment_layout)
+class DiaryHomeFragment: Fragment(R.layout.diary_fragment_layout){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fab: View = view.findViewById(R.id.add_new_entry_button)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here is a snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
+    }
+}
